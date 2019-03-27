905cb952ee115842007120e073158fb1abdf3892
Gonzalo Hirsch <hirschgonzalo@yahoo.com.ar>
2019-03-27 10:50:52 -0300

    i fucked up

--- Algoritmos/src/main/java/DocumentSearch/Main.java
+++ Algoritmos/src/main/java/DocumentSearch/Main.java
@@ -1,4 +1,13 @@
 package DocumentSearch;
 
 public class Main {
+
+    public static void main(String[] args){
+        try{
+            FullTextRetrieval.createIndex();
+            FullTextRetrieval.searchIndexPath("doc1.txt");
+        } catch(Exception ex){
+            System.out.println("Error: " + ex.getMessage());
+        }
+    }
 }
