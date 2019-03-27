811630a1ef253c608e618ab38e900b93b066208e
Gonzalo Hirsch <hirschgonzalo@yahoo.com.ar>
2019-03-27 09:15:04 -0300

    Implementation of IndexOf

---
+++ Algoritmos/src/main/java/IndexOf/Main.java
@@ -0,0 +1,10 @@
+package IndexOf;
+
+public class Main {
+
+    public static void main(String[] args){
+        String a = "abracadabra";
+        String b = "dab";
+        System.out.println("Result: " + IndexOf.IndexOf(a.toCharArray(), b.toCharArray()));
+    }
+}
