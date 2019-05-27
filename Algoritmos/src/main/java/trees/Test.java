4ea7976a08ec72c35974bfd0bbc577b0d9b34ca8
Gonzalo Hirsch <hirschgonzalo@yahoo.com.ar>
2019-05-27 09:37:55 -0300

    Clase 27_5_19

--- Algoritmos/src/main/java/trees/Test.java
+++ Algoritmos/src/main/java/trees/Test.java
@@ -3,6 +3,7 @@ package trees;
 public class Test {
 
     public static void main(String[] args){
+        /*
         BinaryTree<Integer> myTree = new BinaryTree<>();
         myTree.insert(60);
         myTree.insert(50);
@@ -14,5 +15,13 @@ public class Test {
         myTree.insert(20);
 
         System.out.println(myTree.testAVL() ? "YES" : "NO");
+        */
+
+        BTree<Integer> st = new BTree<>(2);
+        for (int rec = 0; rec < 20; rec++){
+            st.add(rec);
+            System.out.println(st.toString());
+            System.out.println("....");
+        }
     }
 }
